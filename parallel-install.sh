ef86d27e7c8687d4e4347c329afc5dd62391ff05
Wanyu Zhang <Wanyu@rcnfs.wanyu-202992.flashburst.emulab.net>
2024-04-18 16:58:32 -0600

    update the spark version

--- parallel-install.sh
+++ parallel-install.sh
@@ -13,5 +13,6 @@ for i in $(seq 2 $NUM_SLAVES); do
  node=$(printf "rc%02d" $((i - 1)))
  # avoid the input of yes/no
  ssh-keyscan -H $node >> ~/.ssh/known_hosts
+ ssh $node "cd ~ && [ -d "spark-env-install/" ] && rm -r "spark-env-install/" "
  ssh $node "cd ~ && git clone https://github.com/wanyuzha/spark-env-install.git && cd spark-env-install && ./install-local.sh $NUM_SLAVES" &
 done
