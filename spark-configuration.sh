8ed9945e5c402551ee569159dc7c5e99f97c8d62
Wanyu Zhang <Wanyu@rcnfs.wanyu-214526.flashburst.emulab.net>
2024-08-22 22:10:36 -0600

    change local ip to dns

--- spark-configuration.sh
+++ spark-configuration.sh
@@ -66,7 +66,7 @@ export SPARK_MASTER_HOST=rcnfs
 export SPARK_MASTER_PORT=7077
 
 # Spark local IP for history server
-export SPARK_LOCAL_IP=127.0.0.1
+export SPARK_PUBLIC_DNS=127.0.0.1
 
 # WebUI port
 export SPARK_MASTER_WEBUI_PORT=8081
