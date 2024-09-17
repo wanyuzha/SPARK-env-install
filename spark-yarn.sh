a954fe460e110951c49b01c854861d73551eaf50
Wanyu Zhang <Wanyu@rcnfs.wanyu-214720.flashburst.emulab.net>
2024-09-16 23:16:24 -0600

    add yarn

--- spark-yarn.sh
+++ spark-yarn.sh
@@ -3,8 +3,7 @@ SPARK_HOME="$HOME/spark"
 
 $SPARK_HOME/bin/spark-submit \
 --master yarn \
---num-executors 50 \
+--num-executors 25 \
 --executor-cores 4 \
 --executor-memory 4g \
---conf spark.executor.extraJavaOptions=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8008 \
 wiki_Q1.py
