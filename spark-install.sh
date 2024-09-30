967181ad6c852848674a8eb25570940068763af1
Wanyu Zhang <Wanyu@rcnfs.wanyu-219348.flashburst.emulab.net>
2024-09-29 18:24:06 -0600

    fix some little error

--- spark-install.sh
+++ spark-install.sh
@@ -23,8 +23,8 @@ pip3 install pyarrow
 INSTALL_DIR="$HOME/spark"
 
 echo "Unzipping Spark..."
-tar -xzf spark-3.4.4-SNAPSHOT-bin-custom-spark.tgz
-rm spark-3.4.4-SNAPSHOT-bin-custom-spark.tgz
-mv spark-3.4.4-SNAPSHOT-bin-custom-spark $INSTALL_DIR
+tar -xzf ~/spark-3.4.4-SNAPSHOT-bin-custom-spark.tgz
+rm ~/spark-3.4.4-SNAPSHOT-bin-custom-spark.tgz
+mv ~/spark-3.4.4-SNAPSHOT-bin-custom-spark $INSTALL_DIR
 
 echo "Spark has been downloaded and unzipped."
